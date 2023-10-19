c8bb9ab8f61f3f79a20c0bab0ccab98b1ddcd596
Balinskiy <3879342@gmail.com>
2023-10-19 13:44:22 +0300

    upd-task-01_&_02

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,5 @@
-
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
 const bodyColor = document.querySelector('body');
@@ -12,20 +11,67 @@ btnStop.addEventListener('click', handlerStop);
 
 let intervalID;
 
-function handlerStart() {
-    if (!intervalID) {
-        btnStart.disabled = true;
-        btnStop.disabled = false;
-        intervalID = setInterval(() => {
-            const randomColor = getRandomHexColor();
-            bodyColor.style.backgroundColor = randomColor;
-        }, 1000)
-    }
+function btnDisabled(disable) {
+  return disable;
 }
-    
+
+function handlerStart() {
+  if (!intervalID) {
+    btnStart.disabled = btnDisabled(true);
+    btnStop.disabled = !btnDisabled(true);
+    intervalID = setInterval(() => {
+      const randomColor = getRandomHexColor();
+      bodyColor.style.backgroundColor = randomColor;
+    }, 1000);
+  }
+}
+
 function handlerStop() {
   clearInterval(intervalID);
   intervalID = null;
-    btnStart.disabled = false; 
-    btnStop.disabled = true;
+  btnStart.disabled = !btnDisabled(true);
+  btnStop.disabled = btnDisabled(true);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// 2 variant
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+// }
+
+// const bodyColor = document.querySelector('body');
+// const btnStart = document.querySelector('button[data-start]');
+// const btnStop = document.querySelector('button[data-stop]');
+
+// btnStart.addEventListener('click', handlerStart);
+// btnStop.addEventListener('click', handlerStop);
+
+// let intervalID;
+
+// function handlerStart() {
+//   btnStart.disabled = true;
+//   btnStop.disabled = false;
+//   intervalID = setInterval(() => {
+//     const randomColor = getRandomHexColor();
+//     bodyColor.style.backgroundColor = randomColor;
+//         }, 1000)
+// }
+    
+// function handlerStop() {
+//   clearInterval(intervalID);
+//   intervalID = null;
+//     btnStart.disabled = false;
+//     btnStop.disabled = true;
+// }
+
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -20,10 +20,10 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    if (selectedDate <= options.defaultDate) {
+  onClose([selectedDate]) {
+    if (selectedDate[0] <= options.defaultDate) {
       alert('Please choose a date in the future');
+      refs.button.disabled = true;
     } else {
       refs.button.disabled = false;
     }
@@ -34,6 +34,13 @@ const options = {
 flatpickr(refs.calendar, options);
 
 let intervalId = null;
+
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  refs.day.textContent = addLeadingZero(days);
+  refs.hour.textContent = addLeadingZero(hours);
+  refs.minute.textContent = addLeadingZero(minutes);
+  refs.second.textContent = addLeadingZero(seconds);
+}
 
 function startCountdown() {
   clearInterval(intervalId); 
@@ -51,10 +58,7 @@ function startCountdown() {
 
     const { days, hours, minutes, seconds } = convertMs(diff);
 
-    refs.day.textContent = addLeadingZero(days);
-    refs.hour.textContent = addLeadingZero(hours);
-    refs.minute.textContent = addLeadingZero(minutes);
-    refs.second.textContent = addLeadingZero(seconds);
+    updateTimerDisplay(days, hours, minutes, seconds);
   };
 
   updateCountdown();
@@ -77,6 +81,104 @@ function convertMs(ms) {
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+
+// const refs = {
+//   calendar: document.querySelector('#datetime-picker'),
+//   button: document.querySelector('button[data-start]'),
+//   day: document.querySelector('span[data-days]'),
+//   hour: document.querySelector('span[data-hours]'),
+//   minute: document.querySelector('span[data-minutes]'),
+//   second: document.querySelector('span[data-seconds]'),
+// };
+
+// refs.button.addEventListener('click', startCountdown);
+
+// let TIMEDEDLINE = null;
+// refs.button.disabled = true;
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     const selectedDate = selectedDates[0];
+//     if (selectedDate <= options.defaultDate) {
+//       alert('Please choose a date in the future');
+//       refs.button.disabled = true;
+//     } else {
+//       refs.button.disabled = false;
+//     }
+//     TIMEDEDLINE = selectedDate;
+//   },
+// };
+
+// flatpickr(refs.calendar, options);
+
+// let intervalId = null;
+
+// function startCountdown() {
+//   clearInterval(intervalId); 
+//   refs.button.disabled = true;
+
+//   const updateCountdown = () => {
+//     const currentTime = Date.now();
+//     const diff = TIMEDEDLINE - currentTime; 
+
+//     if (diff <= 0) {
+//       clearInterval(intervalId);
+//       alert('Time is out')
+//       return;
+//     }
+
+//     const { days, hours, minutes, seconds } = convertMs(diff);
+
+//     refs.day.textContent = addLeadingZero(days);
+//     refs.hour.textContent = addLeadingZero(hours);
+//     refs.minute.textContent = addLeadingZero(minutes);
+//     refs.second.textContent = addLeadingZero(seconds);
+//   };
+
+//   updateCountdown();
+//   intervalId = setInterval(updateCountdown, 1000);
+// }
+
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+//   const days = Math.floor(ms / day);
+//   const hours = Math.floor((ms % day) / hour);
+//   const minutes = Math.floor((ms % hour) / minute);
+//   const seconds = Math.floor((ms % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
 
 
 
