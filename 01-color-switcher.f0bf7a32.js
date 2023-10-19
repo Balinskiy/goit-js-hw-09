07df03b1a02f60750afcd2602dd3d53ef0d41f89
Balinskiy <135216625+Balinskiy@users.noreply.github.com>
2023-10-19 10:45:00 +0000

    Deploying to gh-pages from @ Balinskiy/goit-js-hw-09@c8bb9ab8f61f3f79a20c0bab0ccab98b1ddcd596 🚀

--- 01-color-switcher.f0bf7a32.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let n;e.addEventListener("click",(function(){n||(e.disabled=!0,d.disabled=!1,n=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=e}),1e3))})),d.addEventListener("click",(function(){clearInterval(n),n=null,e.disabled=!1,d.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.f0bf7a32.js.map
